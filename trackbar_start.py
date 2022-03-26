7d0757489ef92d17b4d9e217b92ad4dfea0d5e7a
furkantahabademci <tahafurkanbademci@gmail.com>
2022-03-26 19:43:51 +0300

    HSV Renk Değerlerini Görme

--- trackbar_start.py
+++ trackbar_start.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import*
-from frame_work import Ui_MainWindow
+from framework import Ui_MainWindow
 from PyQt5.QtGui import*
 from PyQt5.QtCore import*
 from PyQt5.QtWidgets import*
